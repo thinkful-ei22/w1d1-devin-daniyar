70a44bc6c72b674f1b256e2fba2be445785de592
Daniyar Salikhov <salihovdr@gmail.com>
2018-06-11 15:11:09 -0400

    Completed drills 1.5

--- drills1.js
+++ drills1.js
@@ -1,9 +1,14 @@
 'use strict';
 
 function whoAmI(name, age){
-  let yearOfBirth = 2018 - age;
+  let yob = yearOfBirth(age);
   console.log('My name is Devin and I\'m 4000 years old');
-  console.log('I was born in ' + yearOfBirth);
+  console.log('I was born in ' + yob);
 }
 
 whoAmI('John',29);
+
+function yearOfBirth(age) {
+  return 2018 - age;
+}
+
