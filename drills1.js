707f514ec7482318564b747cd902bdebd6402cbe
Daniyar Salikhov <salihovdr@gmail.com>
2018-06-11 15:23:10 -0400

    Completed drills 1.6

--- drills1.js
+++ drills1.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function whoAmI(name, age){
-  let yob = yearOfBirth(age);
+  let yob;
+  try {
+    yob = yearOfBirth(age);
+  }
+  catch (e) {
+    console.log (e.message);
+    return;
+  }
+  
   console.log('My name is Devin and I\'m 4000 years old');
   console.log('I was born in ' + yob);
 }
@@ -9,6 +17,9 @@ function whoAmI(name, age){
 whoAmI('John',29);
 
 function yearOfBirth(age) {
+  if (age < 0){
+    throw new Error('Age can not be negative');
+  }
   return 2018 - age;
 }
-
+whoAmI('John', -5);
