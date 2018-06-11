79027b20742ffd113c7bf840210513dfcbf7ae4d
dghero <devingreyhero@gmail.com>
2018-06-11 14:55:24 -0500

    Complete Beyond

--- drills2.js
+++ drills2.js
@@ -6,3 +6,25 @@ function jediName(firstName, lastName){
 }
 
 console.log(jediName('Juan', 'Magnifico'));
+
+function beyond(num){
+  if(num === Infinity || num === -Infinity){
+    console.log('And beyond!');
+  }else if(num > 0){
+    console.log('To infinity...');
+  }else if(num < 0){
+    console.log('To negative infinity...');
+  }else if(num === 0){
+    console.log('Staying home.');
+  }else{
+    //default
+  }
+
+}
+
+beyond(Infinity);
+beyond(-Infinity);
+beyond(5);
+beyond(-5);
+beyond(0);
+beyond('HAHAHA I AM USING THE INTERNET');
